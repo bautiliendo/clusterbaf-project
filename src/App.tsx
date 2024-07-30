38eb229d80bf831f2b7973ac692e415abc04f3ca
bautiliendo <juanbautistaliendo1@gmail.com>
2024-07-29 23:14:05 -0300

    Routing

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,15 @@ import { NavBar } from "./common/NavBar";
 import { Footer } from "./common/Footer";
 import { ButtonWsp } from "./common/ButtonWsp";
 import { HomePage } from './common/HomePage';
+import { IngenieriaGestion } from './components/IngenieriaGestion';
+import { IngenieriaProcesos } from './components/IngenieriaProcesos';
+import { Industrializacion } from './components/Industrializacion';
+import { Mantenimiento } from './components/Mantenimiento';
+import { Outsourcing } from './components/Outsorcing';
+import { Capacitaciones } from './components/Capacitaciones';
+import { About } from './components/About';
+import { Contact } from './components/Contact';
+import { IngYServ } from './components/IngYServ';
 
 
 const App: React.FC = () => {
@@ -14,11 +23,15 @@ const App: React.FC = () => {
       <NavBar />
       <Routes>
         <Route path="/" element={<HomePage />} />
-        {/* <Route path="/about" element={<Navigate to="/#about" replace />} />
-        <Route path="/services" element={<Navigate to="/#services" replace />} />
-        <Route path="/brands" element={<Navigate to="/#brands" replace />} /> */}
-        {/* <Route path="/contact" element={<Navigate to="/#contact" replace />} /> */}
-        {/* Añade más rutas según sea necesario */}
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/ingYServ" element={<IngYServ />} />
+        <Route path="/ingenieriaGestion" element={<IngenieriaGestion />} />
+        <Route path="/ingenieriaProcesos" element={<IngenieriaProcesos />} />
+        <Route path="/industrializacion" element={<Industrializacion />} />
+        <Route path="/mantenimiento" element={<Mantenimiento />} />
+        <Route path="/outsourcing" element={<Outsourcing />} />
+        <Route path="/capacitaciones" element={<Capacitaciones />} />
       </Routes>
       <ButtonWsp />
       <Footer />
