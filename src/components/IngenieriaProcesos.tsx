877f98683deb07bb62f4dbddee67636e5a9d034c
bautiliendo <juanbautistaliendo1@gmail.com>
2024-07-30 11:08:27 -0300

    Sección Ing procesos

--- src/components/IngenieriaProcesos.tsx
+++ src/components/IngenieriaProcesos.tsx
@@ -39,35 +39,39 @@ export const IngenieriaProcesos = () => {
               ))}
             </ul>
 
-            <h3 className="text-3xl font-extrabold text-rich_black-500 sm:text-4xl mt-10">
-              Metodología aplicada
-            </h3>
-
-            <img
-              className="w-full rounded-lg shadow-xl"
-              src={tecnicasLean}
-              alt="Técnicas Lean"
-            />
-
-            <h4 className="text-2xl font-extrabold text-rich_black-500 sm:text-3xl mt-10">
-              Beneficios
-            </h4>
-            <ul className="space-y-4">
-              <li className="flex items-start">
-                <CheckCircle className="w-6 h-6 mr-2 text-jonquil-500 flex-shrink-0 mt-1" />
-                <p className="text-base text-rich_black-400">Se exploran las causas y efectos de problemas transversales en los procesos de forma integral</p>
-              </li>
-              <li className="flex items-start">
-                <CheckCircle className="w-6 h-6 mr-2 text-jonquil-500 flex-shrink-0 mt-1" />
-                <p className="text-base text-rich_black-400">Se crea un plan de acción acorde a las necesidades y problemas específicos del cliente</p>
-              </li>
-              <li className="flex items-start">
-                <CheckCircle className="w-6 h-6 mr-2 text-jonquil-500 flex-shrink-0 mt-1" />
-                <p className="text-base text-rich_black-400">Se logra la estandarización de las acciones para convertirlas en parte operativa de la empresa</p>
-              </li>
-            </ul>
           </div>
         </div>
+      </div>
+      <div className='px-4 mx-auto sm:px-6 lg:px-8 max-w-7xl py-10'>
+        <h3 className="text-3xl font-extrabold text-rich_black-500 sm:text-4xl mt-10 py-4">
+          Metodología aplicada
+        </h3>
+        <div className='relative w-full max-w-2xl'>
+
+          <img
+            className="w-full rounded-lg shadow-xl"
+            src={tecnicasLean}
+            alt="Técnicas Lean"
+          />
+        </div>
+
+        <h4 className="text-2xl font-extrabold text-rich_black-500 sm:text-3xl mt-10 py-4">
+          Beneficios
+        </h4>
+        <ul className="space-y-4">
+          <li className="flex items-start">
+            <CheckCircle className="w-6 h-6 mr-2 text-jonquil-500 flex-shrink-0 mt-1" />
+            <p className="text-base text-rich_black-400">Se exploran las causas y efectos de problemas transversales en los procesos de forma integral</p>
+          </li>
+          <li className="flex items-start">
+            <CheckCircle className="w-6 h-6 mr-2 text-jonquil-500 flex-shrink-0 mt-1" />
+            <p className="text-base text-rich_black-400">Se crea un plan de acción acorde a las necesidades y problemas específicos del cliente</p>
+          </li>
+          <li className="flex items-start">
+            <CheckCircle className="w-6 h-6 mr-2 text-jonquil-500 flex-shrink-0 mt-1" />
+            <p className="text-base text-rich_black-400">Se logra la estandarización de las acciones para convertirlas en parte operativa de la empresa</p>
+          </li>
+        </ul>
       </div>
     </section>
   );
