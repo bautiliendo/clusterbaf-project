f0e99b99d5101470ecd2cae30c3cdb618dbfa78e
bautiliendo <juanbautistaliendo1@gmail.com>
2024-08-05 12:29:56 -0300

    fix z

--- src/common/ButtonWsp.tsx
+++ src/common/ButtonWsp.tsx
@@ -8,7 +8,7 @@ export const ButtonWsp = () => {
             className='fixed bottom-4 right-4
          bg-green-500 text-white p-4
           rounded-full shadow-lg hover:bg-green-600
-           transition-colors duration-300'
+           transition-colors duration-300 z-10'
             title='Ir a WhatsApp'
         >
             <FaWhatsapp size={30} />
